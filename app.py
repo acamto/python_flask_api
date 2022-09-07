f07a69954cede562b0ce1bcb472d9f26d238fb0d
acamto <blackbear016@naver.com>
2022-09-07 10:38:40 +0900

    2022 09 07 alamst josn 추가

--- app.py
+++ app.py
@@ -8,9 +8,22 @@ def hello_world():
     return 'Hello, healthall '
 
 @app.route('/apitest', methods = ['POST'])
-def userLogin():
-    user = request.get_json()#json 데이터를 받아옴
-    return jsonify(user)# 받아온 데이터를 다시 전송
+def postTest():
+    print(request.is_json)
+    apiTest = request.get_json()#json 데이터를 받아옴
+    print(apiTest['alamst_010'])
+    print(apiTest['alamst_020'])
+    return (apiTest['alamst_010'] + "," + apiTest['alamst_020'] 
+    + "\n" + apiTest['jsonTest']['test1'] + "\n" 
+    + apiTest['jsonTest']['test2'])# 받아온 데이터를 다시 전송
+
+'''
+@app.route('/stringTest', methods = ['POST'])
+def stringTest():
+    stringData = request.values
+    return stringData
+'''
+    
  
 @app.route('/getmessage/<message>')
 def environments(message):
